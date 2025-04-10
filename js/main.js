0d8370cc2703f8d400ff63bd77ab8d028a1690bd
CarooZaav24 <carolineglezav19@gmail.com>
2025-04-10 09:40:50 -0600

    Ejercicios completos hasta blur y mayúsculas

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ let encabezado2= document.getElementById("encabezado2");
 let listas = document.getElementsByTagName("ul");
 let elementos = document.getElementsByClassName("list-group-item");
 let btnMostrar= document.getElementById("btnMostrar");
+let txtRFC =document.getElementById("txtRFC");
+let txtTelefono = document.getElementById("txtTelefono");
+let txtCURP = document.getElementById("txtCURP");
 
 encabezado1.innerText="Ejercicio DOM";
 console.log(encabezado1.innerText);
@@ -56,3 +59,43 @@ listas.item(1).insertAdjacentHTML("beforeend",`<li class=list-group-item>Before 
 //el AddEventListener es como agregarle una oreja al elemento
 //con e event.preventDefault le digo a un determinado evento que no haga lo que hace por default.
 //Uso una función anónima cuando la voyu a usar una vez, ey una función normal cuando voy a reutilizar el código.
+
+
+//se ejecuta cuando termina de cargar todos los elementos de la página: 
+window.addEventListener("load", function(event){
+    console.log("Se terminó de cargar la página");
+});// load
+
+ //blur= cuando se sale del campo, cuando pierde el foco, Cuando nos movemos a otra sección
+ //toUpperCase = convertir algo a mayúsculas
+ //target = el origen de unn evento, hace referencia al objeto en donde está sucediento el evento. 
+
+// txtRFC.addEventListener("blur",function(event){
+//     txtRFC.value = txtRFC.value.toUpperCase();
+// }); //txtRFC
+
+// txtCURP.addEventListener("blur", function(event){
+//     event.preventDefault();
+//     txtCURP.value = txtCURP.value.toUpperCase();
+// }); //txtCURP
+
+// txtTelefono.addEventListener("blur", function(evento){
+//     event.preventDefault();
+//     txtTelefono.value = txtTelefono.value. trim().slice(0,10);
+
+// }); txtTelefono
+
+// TODO LO ANTERIOR ( DESDE txtRFC hasta txtTelefono) SE PUEDE MEJORAR U AUTOMATIZAR AÚN MÁS DE LA SIGUIENTE FORMA: 
+function txtToUpper(event){
+    event.preventDefault();
+    event.target.value = event.target.value.trim().toUpperCase();
+
+}// txtToUpper
+
+//El .trim elimima los espacios en blanco al principio y al final de una cadena de texto. .0
+txtRFC.addEventListener("blur", txtToUpper); //txtRFC
+txtCURP.addEventListener("blur", txtToUpper);// txtCURP
+txtTelefono.addEventListener("blur", function(event)
+    {event.preventDefault();
+    txtTelefono.value = txtTelefono.value. trim().slice(0,10);
+});//txtTelefono
